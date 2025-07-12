697eca27298519e1645cec572c47fe9405d95841
Kelvin Muza <k1muza@gmail.com>
2025-07-12 18:03:52 +0200

    fix build error

--- src/app/(admin)/admin/products/[id]/edit/page.tsx
+++ src/app/(admin)/admin/products/[id]/edit/page.tsx
@@ -28,7 +28,6 @@ export default function EditProductPage({ params }: { params: { id: string } }) 
 
   return (
     <div>
-      <h1 className="text-2xl font-bold text-white mb-6">Edit Product</h1>
       {product && <ProductForm product={product} />}
     </div>
   );
--- src/app/api/ingredients/route.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { getIngredients } from "@/data/ingredients";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const ingredients = getIngredients();
-  return NextResponse.json(ingredients);
-}
