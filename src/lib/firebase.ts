f594c26b1c7ef8424ec665df1d67e758040d5e99
kelvin muza <k1muza@gmail.com>
2025-07-14 19:14:28 +0000

    I am getting this warning Database lives in a different region. Please c

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  databaseURL: "https://feedz-99412-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
 // Initialize Firebase
