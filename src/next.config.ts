22ae1d38b6e80e9803e31928dac60ef34bc99624
kelvin muza <k1muza@gmail.com>
2025-07-18 21:47:08 +0000

    ./node_modules/@firebase/database-compat/dist/index.standalone.js:12:1 M

---
+++ src/next.config.ts
@@ -0,0 +1,43 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dy15acotyf9k1.cloudfront.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // This is the new part to fix the Firebase issue.
+    // We are telling webpack to not bundle these modules for the client.
+    if (!isServer) {
+      config.resolve.fallback = {
+        "fs": false,
+        "net": false,
+        "tls": false,
+        "dns": false
+      }
+    }
+
+    return config;
+  }
+};
+
+export default nextConfig;
