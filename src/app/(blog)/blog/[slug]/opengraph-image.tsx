bd781f6e3a71027a52215a9e2e66811ee143bf59
Kelvin Muza <k1muza@gmail.com>
2025-07-19 01:26:17 +0200

    fix opengraph tsx

--- src/app/(blog)/blog/[slug]/opengraph-image.tsx
+++ src/app/(blog)/blog/[slug]/opengraph-image.tsx
@@ -2,13 +2,23 @@ import { ImageResponse } from 'next/og'
 import { getPostBySlug } from '@/app/actions'
 
 export const runtime = 'nodejs'
- 
+
 export default async function Image({ params }: { params: { slug: string } }) {
   const post = await getPostBySlug(params.slug)
- 
+
   return new ImageResponse(
     (
-      <img src={post?.image} alt="Post image" />
+      <img
+        src={post?.image}
+        alt={post?.title}
+        style={{
+          objectFit: 'cover',
+          width: '100%',
+          height: '100%',
+          transform: 'scale(1.05)',
+          transition: 'transform 0.5s'
+        }}
+      />
     )
   )
 }
