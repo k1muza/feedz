86aee0eb83b32a398fb350f65c5f1de62ea2a454
Kelvin Muza <k1muza@gmail.com>
2025-07-19 01:18:09 +0200

    fix opengraph tsx

--- src/app/(blog)/blog/[slug]/opengraph-image.tsx
+++ src/app/(blog)/blog/[slug]/opengraph-image.tsx
@@ -8,15 +8,7 @@ export default async function Image({ params }: { params: { slug: string } }) {
  
   return new ImageResponse(
     (
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <img src={post?.image} height="100" />
-      </div>
+      <img src={post?.image} alt="Post image" />
     )
   )
 }
