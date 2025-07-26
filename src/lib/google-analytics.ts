d4af71765c0abd12a8d488a97a3dc8210ea92cdf
kelvin muza <k1muza@gmail.com>
2025-07-26 17:32:02 +0000

    use analytics env vars

--- src/lib/google-analytics.ts
+++ src/lib/google-analytics.ts
@@ -4,19 +4,19 @@ if (!process.env.GOOGLE_ANALYTICS_PROPERTY_ID) {
     console.warn("GOOGLE_ANALYTICS_PROPERTY_ID is not set. Analytics features will be disabled.");
 }
 
-if (!process.env.GOOGLE_CLIENT_EMAIL || !process.env.GOOGLE_PRIVATE_KEY) {
-    console.warn("GOOGLE_CLIENT_EMAIL or GOOGLE_PRIVATE_KEY is not set. Analytics features will be disabled.");
+if (!process.env.GOOGLE_ANALYTICS_CLIENT_EMAIL || !process.env.GOOGLE_ANALYTICS_PRIVATE_KEY) {
+    console.warn("GOOGLE_ANALYTICS_CLIENT_EMAIL or GOOGLE_ANALYTICS_PRIVATE_KEY is not set. Analytics features will be disabled.");
 }
 
 // The private key from the JSON file has newline characters (\n).
 // When storing it in an environment variable, these can be lost or encoded.
 // The common practice is to replace them with a literal `\\n` in your .env file
 // and then replace them back to `\n` in the code.
-const privateKey = process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+const privateKey = process.env.GOOGLE_ANALYTICS_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
 export const analyticsDataClient = new BetaAnalyticsDataClient({
     credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_email: process.env.GOOGLE_ANALYTICS_CLIENT_EMAIL,
         private_key: privateKey,
     },
 });
