343fcb1a1e73359ac3722ec505a21c6dcd12abee
Kelvin Muza <k1muza@gmail.com>
2025-07-26 23:07:28 +0200

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import "@/app/globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
@@ -78,6 +79,7 @@ export default function RootLayout({
         </AuthProvider>
         <Toaster />
         <Analytics />
+        <SpeedInsights />
       </body>
       <GoogleAnalytics gaId="G-EPHLVQPHS9" />
     </html>
